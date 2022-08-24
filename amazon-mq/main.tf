dd8197247aebac8ddfd83b2f32596af7e6abf942
Sindhu <Sindhusindhuri59@gmail.com>
2022-08-24 13:43:29 +0530

    amazon-mq

---
+++ amazon-mq/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/users/bethapudisindhuri/.aws/credentials"
+  profile                 = "default"
+}
+
+module "vpc" {
+  source  = "cloudposse/vpc/aws"
+  version = "1.1.0"
+
+  cidr_block = "172.16.0.0/16"
+
+  context = module.this.context
+}
+
+module "subnets" {
+  source  = "cloudposse/dynamic-subnets/aws"
+  version = "1.0.0"
+
+  availability_zones   = var.availability_zones
+  vpc_id               = module.vpc.vpc_id
+  igw_id               = module.vpc.igw_id
+  cidr_block           = module.vpc.vpc_cidr_block
+  nat_gateway_enabled  = false
+  nat_instance_enabled = false
+
+  context = module.this.context
+}
+
+module "mq_broker" {
+  source = "../"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.subnets.private_subnet_ids
+
+  allowed_security_group_ids = [module.vpc.vpc_default_security_group_id]
+  allowed_ingress_ports      = [8162, 5671]
+
+  apply_immediately          = var.apply_immediately
+  auto_minor_version_upgrade = var.auto_minor_version_upgrade
+  deployment_mode            = var.deployment_mode
+  engine_type                = var.engine_type
+  engine_version             = var.engine_version
+  host_instance_type         = var.host_instance_type
+  publicly_accessible        = var.publicly_accessible
+  general_log_enabled        = var.general_log_enabled
+  audit_log_enabled          = var.audit_log_enabled
+  kms_ssm_key_arn            = var.kms_ssm_key_arn
+  encryption_enabled         = var.encryption_enabled
+  kms_mq_key_arn             = var.kms_mq_key_arn
+  use_aws_owned_key          = var.use_aws_owned_key
+
+ #ssm_path = "eg/mq/${var.attributes[0]}"
+
+  security_group_create_before_destroy = true
+
+  context = module.this.context
+}
